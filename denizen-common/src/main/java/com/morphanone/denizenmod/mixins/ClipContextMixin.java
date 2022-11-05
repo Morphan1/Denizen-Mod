0bf2de73f9ae52a214bc7980df451c3eefe9d4c5
Morphan1 <morphanone@gmail.com>
2022-11-05 14:49:17 -0400

    Document ClipContextMixin justification

--- denizen-common/src/main/java/com/morphanone/denizenmod/mixins/ClipContextMixin.java
+++ denizen-common/src/main/java/com/morphanone/denizenmod/mixins/ClipContextMixin.java
@@ -9,8 +9,19 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClipContext.class)
 public abstract class ClipContextMixin {
+    /**
+     * <pre>
+     * Normal usage of ClipContext's constructor requires a non-null entity due to its direct
+     * call to {@link CollisionContext#of(Entity)}. This is undesirable for its use in {@link com.morphanone.denizenmod.utilities.RayTrace},
+     * where it is possible no entity is involved.
+     *
+     * It is unlikely other mods will perform anything other than the same modification to this particular line.
+     *
+     * Last verified: 1.19.2
+     * </pre>
+     */
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"))
-    private CollisionContext collisionContextOf(Entity entity) {
+    private CollisionContext denizen_collisionContextOf(Entity entity) {
         return entity != null ? CollisionContext.of(entity) : CollisionContext.empty();
     }
 }
